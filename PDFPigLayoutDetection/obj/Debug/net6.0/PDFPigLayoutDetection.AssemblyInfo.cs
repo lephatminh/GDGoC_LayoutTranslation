af4459f7ec4cde79687bae417f19efe404c0ea8d
OHonSam <ohsam22@apcs.fitus.edu.vn>
2025-05-14 12:38:52 +0700

    chore: remove redundant packages

--- PDFPigLayoutDetection/obj/Debug/net6.0/PDFPigLayoutDetection.AssemblyInfo.cs
+++ PDFPigLayoutDetection/obj/Debug/net6.0/PDFPigLayoutDetection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDFPigLayoutDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b4447e5829ad736db198c17f2a4df76e15e2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8666e25d061aa50e89087515de979bca2f8191a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDFPigLayoutDetection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDFPigLayoutDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
