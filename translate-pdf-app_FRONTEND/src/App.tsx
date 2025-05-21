1beeda0fdf53810e8bcb50ed5c16bb8b89373470
vinhnamhai321 <thanhdc677@gmail.com>
2025-05-21 21:23:58 +0700

    Rebase

--- translate-pdf-app_FRONTEND/src/App.tsx
+++ translate-pdf-app_FRONTEND/src/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import UploadPage from './pages/UploadPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import UploadPage from "./pages/UploadPage";
 import ViewPage from "./pages/ViewPage";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const App: React.FC = () => {
-	return (
-		<>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/upload" element={<UploadPage />} />
-				<Route path="/viewer" element={<ViewPage />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/viewer" element={<ViewPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
